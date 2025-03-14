267d63d94d8d0660f0017bc6b481efe34ad8749a
theshade24 <ifra.mahmood24@gmail.com>
2025-03-14 14:50:07 +0000

    tiles game basic setup

---
+++ index.js
@@ -0,0 +1,37 @@
+let order = [1,4,13,16,2,3,5,6,7,8,9,10,11,12,14,15];
+let pressedOrder = [];
+
+window.onload = function () {
+    changeColour();
+};
+
+function changeColour() {
+    order.forEach((i, idx) => {
+        setTimeout(() => {
+            let currentButton = document.getElementById(`key${i}`);
+            currentButton.style.backgroundColor = "red";
+            setTimeout(() => {
+                currentButton.style.backgroundColor = "grey";
+            }, 300)
+        }, idx*500);
+    });
+}
+
+let buttons = document.querySelectorAll(".key");
+
+buttons.forEach(button => {
+    button.addEventListener("click", function(event) {
+        let keyNumber = Number(event.target.id.slice(3));
+        console.log("keyNumber", keyNumber);
+        pressedOrder.push(keyNumber);
+        console.log(order, pressedOrder);
+    });
+})
+
+function checkWin() {
+    if (JSON.stringify(pressedOrder) == JSON.stringify(order)){
+        window.alert("You win!");
+    } else {
+        window.alert("Try again!");
+    }
+}
